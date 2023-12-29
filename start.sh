e29f6719951da7abea442b6c02531261610c6fda
lelemka0 <20322043+lelemka0@users.noreply.github.com>
2023-12-29 21:19:04 +0800

    feat: Handling `overlay` in `tmpfs` feat: Use `overlay` to handle certificates in `system` optimization: Remove unused code chore: Improve comments

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/system/bin/sh
 APEX_CACERTS_DIR="/apex/com.android.conscrypt/cacerts"
 SYSTEM_CACERTS_DIR="/system/etc/security/cacerts"
-MODULE_CACERTS_DIR="${MODDIR}${SYSTEM_CACERTS_DIR}"
-MODULE_CACERTS_HASH="${MODDIR}/cacerts.sha"
+MODULE_PROCESS_DIR="/dev/custom_ca"
+MODULE_CACERTS_DIR="${MODULE_PROCESS_DIR}/cacerts"
 USER_CACERTS_DIRS="
 /data/misc/user/0/cacerts-custom
 /data/misc/user/0/cacerts-added
 "
 
-function _Calculate_Hash(){
-    echo -n $(find $1 -type f -exec sha256sum {} + | awk '{print $2,$1}' | sort | sha256sum | awk '{print $1}')
-}
 function _Choose_Openssl(){
     # Check CPU ABI and modify PATH
     # Openssl binary from Termux
@@ -29,7 +26,6 @@ function _DER_To_PEM(){
     # The certificates that installed by user, with der format,
     # but the format of all certificates in system root certificate store is "pem" 
     # with plaintext contain the cert in base64 itself and its text below with sha1 fingerprint.
-    # TODO: Convert der to pem
     _Choose_Openssl
     if (( $? != 0 )); then
         echo "No openssl available, skip converting pem."
@@ -41,52 +37,68 @@ function _DER_To_PEM(){
     done
 }
 function _Copy_User_Cacerts(){
+    ###
+    # $1: the source directory to find and copy certificates
+    ###
     for file in $(find $1 -type f); do
         __name=$(echo $file | awk -F'/' '{print $NF}')
         echo $file
         cp $file $MODULE_CACERTS_DIR/$__name
     done
 }
-function _IsChanged(){
-    # TODO: Check whether the certificate has changed to avoid repeated writing.
+function _Setup_Mount(){
+    ###
+    # $1: layer name
+    # $2: mount target
+    ###
+    __OVERLAY="${MODULE_PROCESS_DIR}/mount_${1}"
+    mkdir -p $__OVERLAY/{merged,upper,worker}
+    __MIDDLE_LAYER=$__OVERLAY/merged
+    mount -t overlay "CCA-WORK-${1}" \
+        -o lowerdir=$MODULE_CACERTS_DIR,upperdir=$__OVERLAY/upper,workdir=$__OVERLAY/worker \
+        $__MIDDLE_LAYER
+    [ ! -d "${2}" ] && echo "Fail! mount target: ${2} not exist." && return 1
+    mount -t overlay "CCA-${1}" \
+        -o "lowerdir=$__MIDDLE_LAYER:${2}" \
+        "${2}"
 }
-function Pre_Process(){
-    mkdir -p $MODULE_CACERTS_DIR
-    [ "$(ls -A $MODULE_CACERTS_DIR)" == "" ] || rm $MODULE_CACERTS_DIR/*
-}
-function Post_Process(){
-    _DER_To_PEM
-    chown -R root:root $MODULE_CACERTS_DIR
-    chmod -R ugo-rwx,ugo+rX,u+w $MODULE_CACERTS_DIR
-    chcon -R u:object_r:system_security_cacerts_file:s0 $MODULE_CACERTS_DIR
-    [ "$(ls -A $MODULE_CACERTS_DIR)" == "" ] || touch -t 200901010000.00 $MODULE_CACERTS_DIR/*
-}
-function Post_Process_Apex(){
-    chown -R system:system $1
-    [ "$(ls -A $1)" == "" ] || touch -t 197001010000.00 $1/*
+function Mount_System(){
+    echo "mount system cacerts overlay fs."
+    _Setup_Mount system $SYSTEM_CACERTS_DIR
+    chown -R root:root $__MIDDLE_LAYER
+    chmod -R ugo-rwx,ugo+rX,u+w $__MIDDLE_LAYER
+    chcon -R u:object_r:system_security_cacerts_file:s0 $__MIDDLE_LAYER
+    [ "$(ls -A $__MIDDLE_LAYER)" == "" ] || touch -t 200901010000.00 $__MIDDLE_LAYER/*
 }
 function Mount_Apex(){
+    # In Android 14 (aka API level 34), certificates are now loaded from /apex/com.android.conscrypt/cacerts (instead of /system/etc/security/cacerts).
+    # This new path corresponds to the mounted com.android.conscrypt APEX container, which is signed and immutable.
     [ -d "$APEX_CACERTS_DIR" ] && (
-        echo "!!apex detected. Try to mount overlay fs."
-        MODULE_OVERLAY="${MODDIR}/overlay"
-        rm -r $MODULE_OVERLAY
-        mkdir -p $MODULE_OVERLAY/{layer1,upper,worker}
-        mount -t overlay CCA-Layer1 \
-            -o lowerdir=$MODULE_CACERTS_DIR,upperdir=$MODULE_OVERLAY/upper,workdir=$MODULE_OVERLAY/worker \
-            $MODULE_OVERLAY/layer1
-        mount -t overlay CCA-APEX \
-            -o lowerdir=$MODULE_OVERLAY/layer1:$APEX_CACERTS_DIR \
-            $APEX_CACERTS_DIR
-        Post_Process_Apex $MODULE_OVERLAY/layer1
+        echo "!!apex detected. Try to mount apex cacerts overlay fs."
+        _Setup_Mount apex $APEX_CACERTS_DIR
+        chown -R system:system $__MIDDLE_LAYER
+        chmod -R ugo-rwx,ugo+rX,u+w $__MIDDLE_LAYER
+        chcon -R u:object_r:system_security_cacerts_file:s0 $__MIDDLE_LAYER
+        [ "$(ls -A $__MIDDLE_LAYER)" == "" ] || touch -t 197001010000.00 $__MIDDLE_LAYER/*
     )
+}
+function Pre_Process(){
+    mkdir -p $MODULE_PROCESS_DIR
+    mount -t tmpfs CCA-WORK $MODULE_PROCESS_DIR
+    mkdir $MODULE_CACERTS_DIR
+}
+function Main(){
+    for dir in $USER_CACERTS_DIRS; do
+        [ -d "$dir" ] && _Copy_User_Cacerts $dir
+    done
+    _DER_To_PEM
+    Mount_System
+    Mount_Apex
+}
+function Post_Process(){
+    ###
 }
 
 Pre_Process
-for dir in $USER_CACERTS_DIRS; do
-    [ -d "$dir" ] && _Copy_User_Cacerts $dir
-done
+Main
 Post_Process
-
-# In Android 14 (aka API level 34), certificates are now loaded from /apex/com.android.conscrypt/cacerts (instead of /system/etc/security/cacerts).
-# This new path corresponds to the mounted com.android.conscrypt APEX container, which is signed and immutable.
-Mount_Apex
